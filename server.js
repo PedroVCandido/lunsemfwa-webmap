8abd8a34741314e888ff1ff4ca8abc1d1626cf39
Glitch (hello-express) <none>
2020-09-28 21:30:59 +0000

    🎒🏏 Checkpoint ./server.js:6620615/199 ./app.js copy:6620615/4388 ./app.js:6620615/8776 ./folder/app.js:6620615/4388 ./public/index.html:6620615/1889 ./public/style.css:6620615/1619

--- server.js
+++ server.js
@@ -13,14 +13,9 @@ app.use(express.static("public"));
 
 // https://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
+  response.sendFile(__dirname + "/public/index.html");
 });
 
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
-});
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
