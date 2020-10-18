21551735bb3d56c1a0abb4981cddd24c01b226c7
Glitch (hello-express) <none>
2020-10-18 19:16:38 +0000

    🐏🏢 Checkpoint ./TileService.js:6620615/1611 ./server.js:6620615/1853 ./config.js:6620615/323 ./package.json:6620615/882

--- server.js
+++ server.js
@@ -1,23 +1,63 @@
-// server.js
-// where your node app starts
+"use strict";
 
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require("express");
-const app = express();
+var express = require('express');
+var app = exports.app = express();
+
+var config = require('./config');
+var TileService = require('./TileService');
 
 
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+var routeHandlers = {
+  getTile: function(req, res, next) {
+    var tileService = new TileService(req);
+    tileService.getTile(function(err, tile, headers) {
+      if (err) return res.status(404).send(err.message);
+      res.set(headers);
+      res.send(tile);
+    });
+  },
+  getGrid: function(req, res, next) {
+    var tileService = new TileService(req);
+    tileService.getGrid(function(err, tile, headers) {
+      if (err) return res.status(404).send(err.message);
+      res.set(headers);
+      res.send(tile);
+    });
+  },
+  getInfo: function(req, res, next) {
+    var tileService = new TileService(req);
+    tileService.getInfo(function(err, info) {
+      if (err) return res.status(404).send(err.message);
+      res.json(info);
+    });
+  },
+  ping: function(req, res, next){
+    res.send('tilehut says pong!');
+  },
+  // openshift expects to get a valid response from '/' (health status)
+  healthStatus: function(req, res, next){
+    res.send(':)');
+  },
+};
 
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/public/index.html");
+// TODO Cache-Control: setting
+
+app.disable('x-powered-by');
+app.use('*', function(req, res, next) {
+  // set CORS response header
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  next();
 });
 
+app.use('/:ts/map', express.static(config.MAP_DIR));
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
+app.route('/:ts/:z/:x/:y.*grid.json$').get(routeHandlers.getGrid);
+app.route('/:ts/:z/:x/:y.*').get(routeHandlers.getTile);
+app.route('/:ts/meta.json').get(routeHandlers.getInfo);
+app.route('/ping').get(routeHandlers.ping);
+app.route('/').get(routeHandlers.healthStatus);
+
+app.listen(config.PORT, config.IPADDRESS, function() {
+  console.info('Tilehut on http://%s:%s', config.IPADDRESS, config.PORT);
 });
