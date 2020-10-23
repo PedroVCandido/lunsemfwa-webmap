8bcfcfdd195064c7374b7bdc1db3997a9701be37
Glitch (hello-express) <none>
2020-10-23 10:45:19 +0000

    💾🎐 Checkpoint ./public/app.js:6620615/458 ./public/index.html:6620615/510

--- public/app.js
+++ public/app.js
@@ -1,3 +1,4 @@
+import mapboxgl from 'mapbox-gl';
 
 //Caching the DOM
 const timeFrame_select = document.getElementById("timeframe");
@@ -12,3 +13,11 @@ const legend_div = document.querySelector(".legend");
 
 
 // CONSTRUCTION OF MAP Object
+mapboxgl.accessToken ='pk.eyJ1IjoicGVkcm92aWVpcmFjIiwiYSI6ImNrZnkxODhoNzF3MHYzNnN2MnJ4bWlwZmEifQ.L3drGwHgsf9UeKjP5bzJtg'
+  
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/pedrovieirac/ckfy19zk40s1m19t3xgbvrj59',
+  center: [28,-14.50],
+  zoom: 7
+});
